c001e69d6868e119978cb9c80ace6fb171ae4009
Dek Haji <44979912+dek-haji@users.noreply.github.com>
2024-12-20 17:38:03 -0600

    adding the application required components

---
+++ src/app/features/document/components/document-comments/document-comments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-comments',
+  imports: [],
+  templateUrl: './document-comments.component.html',
+  styleUrl: './document-comments.component.css'
+})
+export class DocumentCommentsComponent {
+
+}
---
+++ src/app/features/document/components/document-section/document-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-section',
+  imports: [],
+  templateUrl: './document-section.component.html',
+  styleUrl: './document-section.component.css'
+})
+export class DocumentSectionComponent {
+
+}
